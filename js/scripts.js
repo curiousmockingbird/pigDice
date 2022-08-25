251d6cb91b50af3640b4091371861f66acf431d9
Harold_Mesa <harold.mesa@icloud.com>
2022-08-25 15:39:00 -0500

    WIP: Adding UI functionalities
    Co-authored-by: Helen Lehrer <hlehrer@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,27 @@ function Player(name, currentRoundScore, totalScore) {
   this.totalScore = totalScore;
 }
 
+//UI Logic
+function handlePlayerFormSubmission (event) {
+  event.preventDefault();
+  let namePlayer1 = document.getElementById("name-player-1").value;
+  let namePlayer2 = document.getElementById("name-player-2").value;
+  
+  let playersDirectory = new PlayersDirectory();
+  let player1 = new Player(namePlayer1, 0, 0);
+  let player2 = new Player(namePlayer2, 0, 0);
+
+  playersDirectory.addPlayer(player1);
+  playersDirectory.addPlayer(player2);
+
+  document.getElementById("players-name").setAttribute("class","hidden");
+  document.getElementById("game").removeAttribute("class","hidden");
+
+  console.log(playersDirectory,player1,player2);
+
+}
+
+window.addEventListener("load", function () {
+  let playerForm = document.getElementById("players-name");
+  playerForm.addEventListener("submit", handlePlayerFormSubmission);
+})
