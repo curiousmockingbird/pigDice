bd526b59caf1ea00e8046bc248548f12755918a0
Harold_Mesa <harold.mesa@icloud.com>
2022-08-25 13:48:01 -0500

    WIP: UI in HTML file
    Co-authored-by: Helen Lehrer <hlehrer@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ function rollDice(minNum, maxNum) {
   minNum = Math.ceil(minNum);
   maxNum = Math.floor(maxNum);
   let diceResult = Math.floor(Math.random() * (maxNum- minNum) + minNum);
-  console.log(diceResult);
   if (diceResult > 1) {
     return diceResult;
   } else { 
@@ -13,15 +12,14 @@ function rollDice(minNum, maxNum) {
 function TotalScore() {
   this.playerScore = {}; 
 }
+
 TotalScore.prototype.addScore = function (rollDice) {
-/*   this.playerScore[player.name] = {};*/
-if (!this.playerScore[player.name]) { 
-/*   rollDice = 0; */
-  this.playerScore[player.name] = rollDice;
-} else {
-  this.playerScore[player.name] += rollDice;
-}
-}
+  if (!this.playerScore[player.name]) { 
+    this.playerScore[player.name] = rollDice;
+  } else {
+    this.playerScore[player.name] += rollDice;
+  }
+};
 
 function PlayersDirectory() {
   this.players ={};
@@ -29,7 +27,7 @@ function PlayersDirectory() {
 
 PlayersDirectory.prototype.addPlayer = function (player) {
   this.players[player.name] = player;
-}
+};
 
 function Player(name, currentRoundScore, totalScore) {
   this.name = name;
